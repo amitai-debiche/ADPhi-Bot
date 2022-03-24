44f107b58d53d76695d56625088d91d8a5b491c8
Amitai <77998669+Rooney13241@users.noreply.github.com>
2022-03-24 17:10:42 +0000

    Initial Commit

---
+++ bot.py
@@ -0,0 +1,16 @@
+import discord
+
+import os
+from dotenv import load_dotenv
+
+
+
+
+
+
+if __name__ == "__main__":
+    #When running this file, if it is the 'main' file
+    #I.E it is not being imported from another python file run this
+
+    bot.run(os.getenv("BOT_TOKEN")
+                   
