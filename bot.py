34f567bc7bb7cb9c17c4c57a924fd702af61db52
Amitai <77998669+Rooney13241@users.noreply.github.com>
2022-03-27 02:23:39 +0000

    Added status changer, changes every couple hours, able to change status through status list

--- bot.py
+++ bot.py
@@ -6,6 +6,7 @@ from dotenv import load_dotenv
 
 #TODO: Switch back to $ when bot is finished, conflicts with current bot
 client = commands.Bot(command_prefix = '!')
+status = cycle(["Status 1","Status 2", "Status 3")
 
 @client.command()
 async def load(ctx, extension):
@@ -20,9 +21,23 @@ for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{os.path.splitext(filename)[0]}')
 
+@client.command()
+async def reload(ctx, extension):
+    client.unload_extension(f'cogs.{extension}')
+    client.load_extension(f'cogs.{extension}')
+
+
 @client.event
 async def on_ready():
+    change_status.start()
     print(client)
+
+#change's bot's game status every couple hours
+@task.loop(seconds=10200)
+async def change_status():
+    await.client.change_presence(activity=discord.Game(next(status)))
+
+
 
 @client.command()
 async def ping(ctx):
