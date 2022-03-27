28657f3ce0e81191e3811901c1e7e2f55a5dc09d
Amitai <77998669+Rooney13241@users.noreply.github.com>
2022-03-27 02:54:08 +0000

    Small python script to automatically download the data on the https links inside songs.json

--- cogs/mp3files/download.py
+++ cogs/mp3files/download.py
@@ -2,7 +2,7 @@ import json
 import requests
 
 def main():
-    with open('/home/ami/src/venv/ADPhi-Bot/cogs/songs.json') as f:
+    with open('./cogs/songs.json') as f:
         data = json.load(f)['songs']
     for x in range(1,11):
         doc = requests.get(data[x]['http'])
